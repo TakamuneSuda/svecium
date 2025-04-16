35c21c2a41987b2136d8c353fe4b75baf1f480ba
TakamuneSuda <40237672+TakamuneSuda@users.noreply.github.com>
2025-04-16 13:16:39 +0900

    add mvt

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,9 +4,10 @@ export { default as Imagery } from './components/Imagery.svelte';
 export { default as Terrain } from './components/Terrain.svelte';
 export { default as Camera } from './components/Camera.svelte';
 export { default as GeoJSONPoint } from './components/GeoJSONPoint.svelte';
-export type { GeoJSONPointProps } from './components/GeoJSONPoint';
 export { default as GeoJSONLine } from './components/GeoJSONLine.svelte';
 export { default as GeoJSONPolygon } from './components/GeoJSONPolygon.svelte';
+export { default as Mvt} from './components/Mvt.svelte';
+export type { GeoJSONPointProps } from './components/GeoJSONPoint';
 
 // Context
 export { type MapContext } from './contexts.svelte.js';
